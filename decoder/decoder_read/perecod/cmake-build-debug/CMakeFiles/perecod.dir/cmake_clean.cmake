1cf89440b8ce7bc62d8d8f7c3f9c56bb1926a7dc
DemidovaZhanna <n_5640@mail.ru>
2020-12-10 14:40:19 +0300

    Add files via upload

---
+++ decoder/decoder_read/perecod/cmake-build-debug/CMakeFiles/perecod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perecod.dir/main.cpp.o"
+  "perecod"
+  "perecod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perecod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
